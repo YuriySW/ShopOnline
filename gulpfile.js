00e5ea6fd46d8e8452a10d91c5c3973ac2bd0aef
YuriySW <yuriy.s@my.com>
2025-02-16 22:17:12 +0500

    favicon work

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,74 @@
+// import gulp from 'gulp';
+// import browserSync from 'browser-sync';
+// import gulpCssimport from 'gulp-cssimport';
+// import {deleteAsync} from 'del';
+// // задачи
+
+// export const html = () => gulp.src('src/*.html').pipe(gulp.dest('dist')).pipe(browserSync.stream());
+
+// export const css = () =>
+//   gulp
+//     .src('src/css/**/*.css')
+//     .pipe(
+//       gulpCssimport({
+//         extensions: ['css'],
+//       })
+//     )
+//     .pipe(gulp.dest('dist/css'))
+//     .pipe(browserSync.stream());
+
+// export const js = () =>
+//   gulp.src('src/script/**/*.js').pipe(gulp.dest('dist/script')).pipe(browserSync.stream());
+
+// export const copy = () =>
+//   gulp
+//     .src(['src/fonts/**/*', 'src/image/**/*'], {
+//       base: 'src',
+//     })
+//     .pipe(gulp.dest('dist'))
+//     .pipe(browserSync.stream({once: true}));
+
+// export const server = () => {
+//   browserSync.init({
+//     ui: false,
+//     notify: false,
+//     //  tunnel: true,
+//     server: {
+//       baseDir: 'dist',
+//     },
+//   });
+
+//   gulp.watch('./src/**/*.html', html);
+//   gulp.watch('./src/css/**/*.css', css);
+//   gulp.watch('./src/script/**/*.js', js);
+//   gulp.watch(['./src/image/**/*', './src/fonts/**/*'], copy);
+// };
+
+// export const clear = (done) => {
+//   return deleteAsync(['dist/**/*'], {
+//     force: true,
+//   }).then(() => done());
+// };
+
+// // запуск
+
+// export const base = gulp.parallel(html, css, js, copy);
+
+// export const build = gulp.series(clear, base);
+
+// export default gulp.series(base, server);
+
 import gulp from 'gulp';
 import browserSync from 'browser-sync';
 import gulpCssimport from 'gulp-cssimport';
 import {deleteAsync} from 'del';
 // задачи
+
+export const favicon = () =>
+  gulp
+    .src('src/favicon/**/*')
+    .pipe(gulp.dest('dist/favicon'))
+    .pipe(browserSync.stream({once: true}));
 
 export const html = () => gulp.src('src/*.html').pipe(gulp.dest('dist')).pipe(browserSync.stream());
 
@@ -42,6 +108,7 @@ export const server = () => {
   gulp.watch('./src/css/**/*.css', css);
   gulp.watch('./src/script/**/*.js', js);
   gulp.watch(['./src/image/**/*', './src/fonts/**/*'], copy);
+  gulp.watch('./src/favicon/**/*', favicon);
 };
 
 export const clear = (done) => {
@@ -52,7 +119,7 @@ export const clear = (done) => {
 
 // запуск
 
-export const base = gulp.parallel(html, css, js, copy);
+export const base = gulp.parallel(html, css, js, copy, favicon);
 
 export const build = gulp.series(clear, base);
 
