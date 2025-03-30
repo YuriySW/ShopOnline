8b533dca6f6c842ebd8ccf0ca5f8e1a151c58874
YuriySW <yuriy.s@my.com>
2025-03-30 23:02:58 +0500

    lessson 7.7 coursework 2.6

--- src/script/modules/basket.js
+++ src/script/modules/basket.js
@@ -1,8 +1,12 @@
-const basketBtn = document.querySelectorAll('.basket-btn');
-const basketTitle = document.querySelector('.basket__title');
-const basketGoodsCount = document.querySelector('.basket__goods-count');
-const BASE_URL = 'https://excited-evanescent-macaroni.glitch.me';
-const DEFAULT_IMAGE = `image/not-img.jpg`;
+import {basketRender, handleImageError} from './createElement.js';
+import {
+  BASE_URL,
+  basketBtn,
+  basketTitle,
+  basketGoodsCount,
+  DEFAULT_IMAGE,
+  innerHTML,
+} from './identifier.js';
 
 basketBtn.forEach((item) => {
   item.addEventListener('click', () => {
@@ -188,17 +192,9 @@ const imageSub = () => {
     `;
 
       basketDeliveryImages.appendChild(listItem);
-      const img = listItem.querySelector('.basket__delivery-image');
 
-      img.onerror = () => {
-        img.src = DEFAULT_IMAGE;
-        const picture = img.closest('picture');
-        if (picture) {
-          picture.querySelectorAll('source').forEach((source) => {
-            source.srcset = DEFAULT_IMAGE;
-          });
-        }
-      };
+      const img = listItem.querySelector('.basket__delivery-image');
+      img.onerror = () => handleImageError(img);
     });
   };
 
@@ -298,103 +294,17 @@ if (window.location.pathname.includes('basket.html')) {
         : '';
       const newPrice = validDiscount ? pricePercent : item.price;
 
-      const itemHtml = `
-     
-               <li class="basket__item" data-id="${item.id}">
+      basketRender(
+        item.id,
+        imageUrl,
+        DEFAULT_IMAGE,
+        item.title,
+        itemGood,
+        newPrice,
+        oldPriceSpan,
+        basketList
+      );
 
-                  <input
-                    class="basket__checkbox basket__checkbox-choose"
-                    type="checkbox"
-                    name="all"
-                    id="choose-product"
-                  />
-                  <div class="basket__item-content">
-                    <div class="basket__product-info">
-                      <picture class="basket__picture">
-                   
-                        <img
-                          loading="lazy"
-                          class="basket__image"
-                              src="${imageUrl}"
-                          alt="ноутбук"
-                          width="420"
-                          height="295"
-                              onerror="this.src='${DEFAULT_IMAGE}'"
-                        />
-                      </picture>
-                      <div class="basket__text">
-                        <h2 class="basket__product-title">
-                          ${item.title}
-                        </h2>
-                  
-                      </div>
-                      <div class="basket__counter">
-                        <svg
-                          class="basket__counter-minus"
-                          aria-label="добавить колличество покупки"
-                          tabindex="0"
-                          width="36"
-                          height="36"
-                          viewBox="0 0 36 36"
-                          fill="none"
-                          xmlns="http://www.w3.org/2000/svg"
-                        >
-                          <circle cx="18" cy="18" r="17.5" fill="white" stroke="#E8E8E8" />
-                          <path d="M14.2 18.088H21.08V19.168H14.2V18.088Z" fill="#8F8F8F" />
-                        </svg>
-                        <span class="basket__counter-number">${itemGood}</span>
-                        <svg
-                          class="basket__counter-plus"
-                          aria-label="уменьшить колличество покупки"
-                          tabindex="0"
-                          width="36"
-                          height="36"
-                          viewBox="0 0 36 36"
-                          fill="none"
-                          xmlns="http://www.w3.org/2000/svg"
-                        >
-                          <circle cx="18" cy="18" r="17.5" fill="white" stroke="#E8E8E8" />
-                          <path
-                            d="M21.48 19.168H18.216V22.608H17.056V19.168H13.808V18.088H17.056V14.672H18.216V18.088H21.48V19.168Z"
-                            fill="#2D2D2D"
-                          />
-                        </svg>
-                      </div>
-                      <div class="basket__price">
-                        <span class="basket__price-current">${newPrice} ₽</span>
-                     ${oldPriceSpan}
-                        <span class="basket__price-credit">В кредит от 5600 ₽ </span>
-                      </div>
-                      <button class="basket__delete-btn basket__delete-btn--sub">
-                        <svg
-                          xmlns="http://www.w3.org/2000/svg"
-                          width="30"
-                          height="30"
-                          viewBox="0 0 30 30"
-                          fill="none"
-                        >
-                          <g clip-path="url(#clip0_217_1895)">
-                            <path
-                              d="M19.0214 5.35355L19.1679 5.5H19.375H23.25V7H6.75V5.5H10.625H10.8321L10.9786 5.35355L12.0821 4.25H17.9179L19.0214 5.35355ZM10 25.75C8.90114 25.75 8 24.8489 8 23.75V9.25H22V23.75C22 24.8489 21.0989 25.75 20 25.75H10Z"
-                              fill="#C9C9C9"
-                              stroke="#C9C9C9"
-                            />
-                          </g>
-                          <defs>
-                            <clipPath id="clip0_217_1895">
-                              <rect width="30" height="30" fill="white" />
-                            </clipPath>
-                          </defs>
-                        </svg>
-                      </button>
-                    </div>
-                  </div>
-                  </li>
-                  <div class="basket__stripe basket__stripe--sub"></div>
-             
-      `;
-
-      basketList.insertAdjacentHTML('beforeend', itemHtml);
       imageSub();
       document.addEventListener('DOMContentLoaded', countGood);
       checkBoxChange();
